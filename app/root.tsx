b798b84ab196d11abf0f00b5965f3512ca03ca5b
Takayyz <like.the.wind0306@gmail.com>
2025-06-14 17:10:31 +0900

    feat: loaderを使ったデータ読み込み処理追加

--- app/root.tsx
+++ app/root.tsx
@@ -7,14 +7,23 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
-import appStylesHref from "./app.css?url"
+import appStylesHref from "./app.css?url";
+import { getContacts } from "./data";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
 ];
 
+export const loader = async () => {
+  const contacts = await getContacts();
+  return Response.json({ contacts });
+};
+
 export default function App() {
+  const { contacts } = useLoaderData();
+
   return (
     <html lang="en">
       <head>
@@ -42,14 +51,26 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <Link to={`/contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-                <Link to={`/contacts/2`}>Your Friend</Link>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`/contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>{contact.first} {contact.last}</>
+                      ) : (
+                        <i>名前なし</i>
+                      )}{" "}
+                      {contact.favorite ? (<span>*</span>) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>連絡先なし</i>
+              </p>
+            )}
           </nav>
         </div>
         <div id="detail">
