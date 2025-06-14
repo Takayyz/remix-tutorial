3a86e01e3481756f3e26f2edcdd48b014342125e
Takayyz <like.the.wind0306@gmail.com>
2025-06-15 00:02:52 +0900

    feat: キャンセルボタンの処理追加

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,9 +1,11 @@
 import { Form, useLoaderData } from "@remix-run/react";
-import type { FunctionComponent } from "react";
 
 import { getContact, type ContactRecord } from "../data";
-import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import invariant from "tiny-invariant";
+
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import type { FunctionComponent } from "react";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   invariant(params.contactId, 'Missing contactId param');
